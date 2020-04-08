5745a5d165645bac005e4d7fad324928394a8f8f
Pedro Algarvio <pedro@algarvio.me>
2020-04-08 18:27:34 +0100

    Untangle `test/integration/shell/test_call.py`

--- tests/support/pytest/helpers.py
+++ tests/support/pytest/helpers.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
     tests.support.pytest.helpers
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -161,7 +160,7 @@ def temp_state_file(name, contents, saltenv="base", strip_first_newline=True):
     """
 
     if saltenv == "base":
-        directory = RUNTIME_VARS.TMP_STATE_TREE
+        directory = RUNTIME_VARS.TMP_BASEENV_STATE_TREE
     elif saltenv == "prod":
         directory = RUNTIME_VARS.TMP_PRODENV_STATE_TREE
     else:
@@ -209,7 +208,7 @@ def temp_pillar_file(name, contents, saltenv="base", strip_first_newline=True):
     """
 
     if saltenv == "base":
-        directory = RUNTIME_VARS.TMP_PILLAR_TREE
+        directory = RUNTIME_VARS.TMP_BASEENV_PILLAR_TREE
     elif saltenv == "prod":
         directory = RUNTIME_VARS.TMP_PRODENV_PILLAR_TREE
     else:
